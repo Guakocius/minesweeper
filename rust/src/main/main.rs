e20df0b0929a0204a2de7af2e3c4d5cc996c0d23
Guakocius <engelhardt.alexander@mein.gmx>
2026-01-30 20:50:52 +0100

    Removed accidental src dir merging of the Rust code

--- rust/src/main/main.rs
+++ rust/src/main/main.rs
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-use color_eyre::Result;
-
-mod observer;
-mod view;
-use crate::view::tui::app::App;
-use crate::view::tui::terminal::Tui;
-
-fn main() -> Result<()> {
-    color_eyre::install()?;
-    let mut terminal = view::tui::terminal::init()?;
-    let app_result = App::default().run(&mut terminal);
-    if let Err(err) = view::tui::terminal::restore() {
-        eprintln!(
-            "failed to restore terminal. Run `reset` or restart your terminal to recover: {err}"
-        );
-    }
-    app_result
-=======
 use std::thread;
 
 use color_eyre::Result;
@@ -43,5 +24,4 @@ fn main() -> Result<()> {
     });
     handle.join().expect("tui thread panicked")?;
     Ok(())
->>>>>>> 166f2dcd293967d29e5536fbfe26fe61898c38e9
 }
